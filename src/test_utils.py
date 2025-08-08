e0cc822c016bdb8b2e9de7ed308becf74439e0e6
zyaeger <zachyaeger@gmail.com>
2025-08-08 15:24:35 -0700

    implement node splitting

---
+++ src/test_utils.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+from utils import split_nodes_delimiter
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_code_case(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+    
+    def test_bold_case(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    # TODO: write more tests
+    def test_link_case(self):
+        assert True
+    
+    def test_img_case(self):
+        assert True
--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,21 @@ def split_nodes_delimiter(
     old_nodes: list[TextNode], delimiter: str, text_type: TextType
 ) -> list[TextNode]:
     new_nodes = []
+    for node in old_nodes:
+        split_nodes = []
+        delim_count = node.text.count(delimiter)
+        if delim_count == 0:
+            split_nodes.append(node)
+        if delim_count % 2 == 1:
+            raise Exception("Invalid Markdown syntax. Corresponding closing delimeter not detected.")
+        split_node_text = node.text.split(delimiter)
+        for idx, text in enumerate(split_node_text):
+            if text == "":
+                continue
+            if idx % 2 == 0:
+                split_nodes.append(TextNode(text, node.text_type))
+            else:
+                split_nodes.append(TextNode(text, text_type))
 
+        new_nodes.extend(split_nodes)
     return new_nodes
