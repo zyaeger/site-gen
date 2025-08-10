662821c7a743dcbd11c0d133df5b9546286c0787
zyaeger <zachyaeger@gmail.com>
2025-08-09 18:31:19 -0700

    implement inline markdown

--- src/utils.py
+++ src/utils.py
@@ -6,20 +6,19 @@ def split_nodes_delimiter(
 ) -> list[TextNode]:
     new_nodes = []
     for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
         split_nodes = []
-        delim_count = node.text.count(delimiter)
-        if delim_count == 0:
-            split_nodes.append(node)
-        if delim_count % 2 == 1:
-            raise Exception("Invalid Markdown syntax. Corresponding closing delimeter not detected.")
         split_node_text = node.text.split(delimiter)
+        if len(split_node_text) % 2 == 0:
+            raise Exception("Invalid Markdown syntax. Corresponding closing delimeter not detected.")
         for idx, text in enumerate(split_node_text):
             if text == "":
                 continue
             if idx % 2 == 0:
-                split_nodes.append(TextNode(text, node.text_type))
+                split_nodes.append(TextNode(text, TextType.TEXT))
             else:
                 split_nodes.append(TextNode(text, text_type))
-
         new_nodes.extend(split_nodes)
     return new_nodes
