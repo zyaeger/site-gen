3ccc1e282bd0932378a1544b093fef8f120bb0cb
zyaeger <zachyaeger@gmail.com>
2025-08-12 16:29:27 -0700

    fixes

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from site-gen!")
-
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -3,22 +3,24 @@ import sys
 from src import page_gen
 
 STATIC_PATH = "./static"
-PUBLIC_PATH = "./public"
-DOCS_PATH = "./docs"
+PUBLIC_PATH = "./docs"
 CONTENT_PATH = "./content"
 TEMPLATE_PATH = "./template.html"
+DEFAULT_BASEPATH = "/"
 
 
 def main():
-    basepath = "/"
-    if len(sys.argv) >= 2:
+    basepath = DEFAULT_BASEPATH
+    if len(sys.argv) > 1:
         basepath = sys.argv[1]
 
     print("Copying static files to docs directory...")
-    page_gen.copy_dir(STATIC_PATH, DOCS_PATH)
+    page_gen.copy_dir(STATIC_PATH, PUBLIC_PATH)
 
     print("Generating content...")
-    page_gen.generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, DOCS_PATH, basepath)
+    page_gen.generate_pages_recursive(
+        CONTENT_PATH, TEMPLATE_PATH, PUBLIC_PATH, basepath
+    )
     print("Done!")
 
 
