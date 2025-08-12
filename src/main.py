af5aacb50eca0ce6d007ce5abe208f63999e2abe
zyaeger <zachyaeger@gmail.com>
2025-08-12 12:31:15 -0700

    add copy_dir function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
-from src.textnode import TextNode, TextType
+import os
+import shutil
+
+
+STATIC_PATH = "./static"
+PUBLIC_PATH = "./public"
 
 
 def main():
-    node = TextNode("Foo", TextType.LINK, "http://example.com")
-    print(node)
+    print("Copying static files to public directory...")
+    copy_dir("./static", "./public")
+    print("Done!")
+
+
+def copy_dir(src_dir: str, dest_dir: str) -> None:
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+        print(f" * {src_path} -> {dest_path}")
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            copy_dir(src_path, dest_path)
 
 
 if __name__ == "__main__":
