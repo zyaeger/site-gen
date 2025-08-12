b0e5d7ee478b5c60333ad956a46f06343222edf4
zyaeger <zachyaeger@gmail.com>
2025-08-11 19:26:33 -0700

    move tests to separate folder + add static files for testing

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,5 @@
 # Runs .github/workflows/pr_check.yml locally
 # Improvement: convert into Makefile
-python3 -m unittest discover -s src -v
+python3 -m unittest src/tests/test_*.py -v -c
 uv run black --check $(git ls-files '*.py')
 uv run pylint $(git ls-files '*.py')
