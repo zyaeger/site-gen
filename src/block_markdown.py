2c7c7721d6e7488a153318ac5c8f5316b5b74d1c
zyaeger <zachyaeger@gmail.com>
2025-08-11 18:40:30 -0700

    implement markdown_to_html_node

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,9 @@
 from enum import StrEnum
 
+from src.htmlnode import HTMLNode, ParentNode
+from src.inline_markdown import text_to_textnodes
+from src.textnode import TextNode, TextType, text_node_to_html_node
+
 
 class BlockType(StrEnum):
     PARAGRAPH = "paragraph"
@@ -40,3 +44,53 @@ def block_to_block_type(text_block: str) -> BlockType:
         return BlockType.OLIST
 
     return BlockType.PARAGRAPH
+
+
+def text_to_children(text: str) -> list[HTMLNode]:
+    text_nodes = text_to_textnodes(text)
+    return [text_node_to_html_node(tn) for tn in text_nodes]
+
+
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        if block_type == BlockType.PARAGRAPH:
+            html_node = ParentNode(
+                "p", children=text_to_children(block.replace("\n", " "))
+            )
+        elif block_type == BlockType.HEADING:
+            header_num = block.count("#")
+            html_node = ParentNode(f"h{header_num}", children=text_to_children(block))
+        elif block_type == BlockType.CODE:
+            node = TextNode(block.replace("```", ""), TextType.CODE)
+            html_node = ParentNode("pre", children=[text_node_to_html_node(node)])
+        elif block_type == BlockType.QUOTE:
+            html_node = ParentNode(
+                "blockquote", children=text_to_children(block.replace(">", ""))
+            )
+        elif block_type == BlockType.ULIST:
+            children = []
+            for line in block.split("\n"):
+                child_node = ParentNode(
+                    "li", children=text_to_children(line.replace("- ", ""))
+                )
+                children.append(child_node)
+            html_node = ParentNode("ul", children=children)
+        elif block_type == BlockType.OLIST:
+            children = []
+            for line in block.split("\n"):
+                child_node = ParentNode(
+                    "li", children=text_to_children(line.split(". ", 1)[1])
+                )
+                children.append(child_node)
+            html_node = ParentNode("ol", children=children)
+        else:
+            raise ValueError("Invalid Block Type")
+
+        children.append(html_node)
+
+    parent_node = ParentNode("div", children=children)
+    return parent_node
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,11 @@
 import unittest
 
-from src.block_markdown import BlockType, block_to_block_type, markdown_to_blocks
+from src.block_markdown import (
+    BlockType,
+    block_to_block_type,
+    markdown_to_blocks,
+    markdown_to_html_node,
+)
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -84,6 +89,38 @@ This is the same paragraph on a new line
         block = "1. This is\n2. a list\n3. that is\n4. ordered"
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, BlockType.OLIST)
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        print(html)
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>\nThis is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
 
 
 if __name__ == "__main__":
